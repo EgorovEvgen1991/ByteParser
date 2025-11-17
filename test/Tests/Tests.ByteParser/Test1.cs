41356355ee84f232bb3617821a8c075225470845
EgorovEvgen1991 <egorov.evgen1991@yandex.ru>
2025-11-17 20:40:57 +0300

    test first commit

---
+++ test/Tests/Tests.ByteParser/Test1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace Tests.ByteParser
+{
+    [TestFixture]
+    public sealed class Test1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+          ClassicAssert.IsTrue(true);
+        }
+    }
+}
